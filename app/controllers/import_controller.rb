c1a6d689323619a0fa7c6517120e36ae8dc80297
Sam Walston <samjwalston@gmail.com>
2018-09-07 20:15:15 -0700

    Trying puts with rescue statement

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,24 +1,27 @@
 class ImportController < ApplicationController
   def teams
     inspect_data
+    head :no_content
   end
 
   def rosters
     inspect_data
+    head :no_content
   end
 
   def schedules
     inspect_data
+    head :no_content
   end
 
 
   private
 
   def inspect_data
+    puts "Hello There"
+  rescue
     puts "==="
     puts request.format
     puts "==="
-
-    head :no_content
   end
 end
