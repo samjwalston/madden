4466d836f3e80d20a691a3005697affaebd70961
Sam Walston <samjwalston@gmail.com>
2019-10-26 08:56:30 -0700

    Commenting gzip parsing

--- config/initializers/params_parser.rb
+++ config/initializers/params_parser.rb
@@ -1,10 +1,10 @@
-module ActionDispatch
-  class Request
-    self.parameter_parsers = {
-      Mime[:json].symbol => -> (raw_post) {
-        data = ActiveSupport::JSON.decode(ActiveSupport::Gzip.decompress(raw_post))
-        data.is_a?(Hash) ? data : { _json: data }
-      }
-    }
-  end
-end
+# module ActionDispatch
+#   class Request
+#     self.parameter_parsers = {
+#       Mime[:json].symbol => -> (raw_post) {
+#         data = ActiveSupport::JSON.decode(ActiveSupport::Gzip.decompress(raw_post))
+#         data.is_a?(Hash) ? data : { _json: data }
+#       }
+#     }
+#   end
+# end
