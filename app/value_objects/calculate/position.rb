8445939041e48f54a51895a96c692e1c4145ea38
Sam Walston <samjwalston@gmail.com>
2021-02-28 10:49:45 -0800

    Tested new structure of quarterback import

---
+++ app/value_objects/calculate/position.rb
@@ -0,0 +1,44 @@
+class Calculate::Position
+  def initialize(**params)
+    @category = (params[:category] || "player")
+    @archetypes = (params[:archetypes] || [])
+    @players = (params[:players] || [])
+  end
+
+  def rating
+    @rating ||= calculate_rating
+  end
+
+  def value
+    @value ||= calculate_value
+  end
+
+  def role
+    @role unless @role.nil?
+    archetype = get_role
+
+    @role = {
+      name: role_name,
+      style: archetype[:name],
+      rating: archetype[:overall_rating],
+      value: calculate_value
+    }
+  end
+
+
+  private
+
+  def get_role
+    @archetypes.to_a.sort{|a, b| b[:overall_rating] <=> a[:overall_rating]}.first
+  end
+
+  def get_roles(player_count, *styles)
+    players = @players.map do |p|
+      archetypes = (styles.empty? ? p.archetypes : p.archetypes.to_a.find_all{|a| a.name.in?(styles)})
+      archetype = archetypes.sort{|a, b| b.overall_rating <=> a.overall_rating}.first
+      archetype.attributes
+    end.sort{|a, b| b["overall_rating"] <=> a["overall_rating"]}
+
+    player_count == 1 ? players.first : players[0...player_count]
+  end
+end
---
+++ app/value_objects/calculate/quarterback.rb
@@ -0,0 +1,29 @@
+class Calculate::Quarterback < Calculate::Position
+  PROSPECT_VALUE = 1.to_d.freeze
+  PLAYER_VALUE = 0.2204.to_d.freeze
+
+
+  private
+
+  def role_name
+    "QB"
+  end
+
+  # Team(players)
+  def calculate_rating
+    get_roles(1)
+  end
+
+  # Player(archetypes)
+  # Prospect(archetypes)
+  # FreeAgency(players)
+  def calculate_value
+    if @category == "player"
+      (get_role[:overall_rating] * PLAYER_VALUE).round(4)
+    elsif @category == "prospect"
+      (get_role[:overall_rating] * PROSPECT_VALUE).round(4)
+    elsif @category == "free_agency"
+      get_roles(1)[:overall_rating].round(2).to_f
+    end
+  end
+end
